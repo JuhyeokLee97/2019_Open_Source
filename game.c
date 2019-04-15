7a8a1914734eb020c4bad35c51b47aff083ce18d
zooh <zero5.two4@gmail.comm>
2019-04-15 22:36:01 +0900

    Apr 15th OSS 주석달기 version 1

--- game.c
+++ game.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4
+#define SIZE 4  // 2048게임에 기본 틀은 4*4형태이므로 바꾸지 않는 것이 좋다.
 
 enum directions {RIGHT, DOWN, LEFT, UP};
 //directions 0 ->, 1 down,  2 <-, 3 up
 
 struct Square {
-  int value;
-  int used;
-};
+  int value;  // 사용자에게 보여주는 data이다.
+  int used;   // value 값을 연산할 때 flag변수로 사용된다.
+};/*2차원 Matrix형태로 square배열이 만들어진다. square하나가 cell 하나와 같다고 생각하면 된다.*/
 
 struct Pos {
   int x;
   int y;
-};
+};/* 2차원 Matrix형태로 sqaure배열의 cell 위치 data를 갖는다. */
 
 void draw(struct Square m[][SIZE]) {
   int i,j;
@@ -23,7 +23,7 @@ void draw(struct Square m[][SIZE]) {
     }
     printf("\n");
   }
-}
+}/* sqaure.value 값을 4*4 Matrix 형태로 콘솔에 출력해준다. */
 
 void rightShiftCell(struct Square arr[], int currentPos) {
   arr[currentPos + 1].value = arr[currentPos].value;
@@ -67,18 +67,29 @@ void rotate(struct Square m[][SIZE]) {
       m[n-j-1][i] = temp;
     }
   }
-}
+}/* 함수 실행 예시 (when i++)
+  ====[Before Matrix]====
+  a11   a12   a13   a14
+  a21   a22   a23   a24
+  a31   a32   a33   a34
+  a41   a42   a43   a44
+  ====[After Matrix]====
+  a14   a24   a34   a44
+  a13   a23   a33   a43
+  a12   a22   a32   a42
+  a11   a21   a31   a41
+*/
 
 void doRotations(struct Square m[][SIZE], int times) {
   int i;
   for (i = 0; i < times; i++) {
     rotate(m);
   }
-};
+};/* Be called by function of 'handleMove'. 파라미터 2차원 배열 m을 times번 반시계 방향으로 회전시킨다. */
 
 void handleMove(int direction, struct Square m[][SIZE]) {
   int i;
-  int times = direction;
+  int times = direction;  // direction파라미터가 정수 값을 갖고, times변수에 반시계방향으로 회전할 횟수를 저장한다.
 
   doRotations(m, times);
   for (i = 0; i < SIZE; i++) {
