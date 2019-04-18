bead7e0ed53547030cb9fbd53f1181901ca29506
zooh <zero5.two4@gmail.comm>
2019-04-18 15:03:10 +0900

    Apr 18th Version 1

--- game.c
+++ game.c
@@ -1,3 +1,15 @@
+///////////////////////////////////////////////////////////////////////
+//  #TODO
+//    1. slideArrayToRight함수 내 mergeCells함수 부분에 대해서 mergeCells함수가 호출 되었을 시 반복문의 인덱스 값에 +1을 해줘야 합쳐진 셀을 한번 더 합쳐지지 않는다.
+//    2. 게임 킬 때 마다 나오는 cell의 모양이 일정하다. 즉 srand()함수를 이용해야 한다.
+//  #TODO/MAYBE-LATER
+//    1. main함수 지역변수 move 가 필요하지 않다 -> 삭제 필요 (BC. memory 낭비)
+//    2. 무한 루프 제거
+//    3. SlideArrayToRight 함수 내 조건문의 조건식 함수화
+//    4. getEmptyCells 함수 내 조건문의 조건식 함수화
+//    5. updateBoardCells 함수 내 조건문의 조건식 함수화
+//
+///////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 4  // 2048게임에 기본 틀은 4*4형태이므로 바꾸지 않는 것이 좋다.
@@ -28,13 +40,22 @@ void draw(struct Square m[][SIZE]) {
 void rightShiftCell(struct Square arr[], int currentPos) {
   arr[currentPos + 1].value = arr[currentPos].value;
   arr[currentPos].value = 0;
-}
+}/* 매개변수 arr : Square 구조체 1차원 배열
+    매개변수 currentPos : 오른쪽으로 이동할 배열 원소의 인덱스
+    반환값 : 없음
+    currentPos위치의 배열 원소를 currentPos + 1위치로 이동한다.
+*/
 
 void mergeCells(struct Square arr[], int initialPos, int finalPos) {
   arr[finalPos].value *= 2;
   arr[finalPos].used = 1;
   arr[initialPos].value = 0;
-}
+}/* 매개변수 arr : Square 구조체 1차원 배열
+    매개변수 initialPos : 옮겨질 Square 구조체 배열 원소의 인덱스
+    매개변수 finalPos : initialPos에서 finalPos 인덱스 위치로 값이 더해진다.
+    반환값 : 없음
+    arr[initialPos].value값을 arr[finalPos].value값에 더한다.
+*/
 
 //only works for right direction, so we need to rotate the board before
 void slideArrayToRight(struct Square arr[]) {
@@ -45,9 +66,9 @@ void slideArrayToRight(struct Square arr[]) {
 
     for(j = i; j < SIZE - 1; j++) {
       if (arr[j+1].used) continue;
-      if (!arr[j+1].value) {
+      if (!arr[j+1].value) {  // case : arr[j].value != 0 && arr[j+1] == 0
         rightShiftCell(arr, j);
-      } else if (arr[j+1].value == arr[j].value) {
+      } else if (arr[j+1].value == arr[j].value) {  // case : arr[j].value != 0 && arr[j+1] != 0
         mergeCells(arr, j, j + 1);
       }
     }
@@ -85,7 +106,10 @@ void doRotations(struct Square m[][SIZE], int times) {
   for (i = 0; i < times; i++) {
     rotate(m);
   }
-};/* Be called by function of 'handleMove'. 파라미터 2차원 배열 m을 times번 반시계 방향으로 회전시킨다. */
+};/*매개변수 time : 반시계 방향으로 회전할 횟수
+    반환값 : 없음
+    Be called by function of 'handleMove'. 
+    파라미터 2차원 배열 m을 times번 반시계 방향으로 90도 회전시킨다. */
 
 void handleMove(int direction, struct Square m[][SIZE]) {
   int i;
@@ -100,7 +124,7 @@ void handleMove(int direction, struct Square m[][SIZE]) {
 
 int generateRandom(int min, int max) {
   return rand()%(max-min+1) + min;
-}
+}/* 매개변수 min과 max의 사이의 임의으 수를 반환한다. */
 
 int getEmptyCells(struct Pos empty[], struct Square m[][SIZE]) {
   int i,j;
@@ -114,17 +138,24 @@ int getEmptyCells(struct Pos empty[], struct Square m[][SIZE]) {
         empty[n++] = temp;
       }
   return n;
-}
+}/* 매개변수 pos empty[] : Square 구조체 배열의 원소 중 value==0인 원소 인덱스 저장
+    2중 반복문을 이용해 구조체 m 배열 원소의 value == 0인 원소를 탐색한다.
+    value == 0인 원소의 인덱스 정보를 empty에 저장한다.
+    반환값 n : Square 구조체 배열에서 value == 0인 원소의 개수
+ */
 
 void addRandomSquare(struct Square m[][SIZE]) {
-  struct Pos empty[SIZE * SIZE];
+  struct Pos empty[SIZE * SIZE];  // Square 구조체 m배열의 원소 중 value == 0인 원소의 인덱스(위치) 저장
   int random;
   int n = getEmptyCells(empty, m);
-  if (n > 0) {
+  if (n > 0) {  
     random = generateRandom(0, n);
-    m[empty[random].x][empty[random].y].value = 2;
+    m[empty[random].x][empty[random].y].value = 2;  // Square구조체 배열 m의 원소 중 value==0인 임의의 원소에 value 변수 '2'로 초기화
   }
-}
+}/* 매개변수 m : Square 구조체 size*size 크기의 배열
+    반환 값 : 없음
+    Square 구조체 배열 m의 value == 0인 임의의 원소에 vlaue 값을 '2'로 초기화
+*/
 
 int playerWon(struct Square m[][SIZE]) {
   int i, j;
@@ -134,7 +165,12 @@ int playerWon(struct Square m[][SIZE]) {
     }
   }
   return 0;
-}
+}/* 매개변수 m : size*size square구조체 2차원 배열
+  2차원 반복문을 통해서 각 원소의 value 값들을 탐색한다.
+  value값이 2048인 경우 '1'을 반환한다.
+  반환값 1 : 게임을 이겼음을 의미
+  반환값 0 : 게임을 끝나지 않았음을 의미
+ */
 
 void updateBoardCells(struct Square m[][SIZE], int value, int used) {
   int i,j;
@@ -152,11 +188,16 @@ void initBoard(struct Square m[][SIZE]) {
   for (i = 0; i < randomSquares; i++) {
     addRandomSquare(m);
   }
-}
+}/* 매개변수 m : 초기화 될 square 구조체 배열
+    반환값 : 없음
+    각 구조체 배열 원소의 멤버 변수 value와 used를 '0'으로 초기화
+*/
 
 void cleanCells(struct Square m[][SIZE]) {
   updateBoardCells(m, -1, 0);
-}
+}/* 매개변수 m : square 구조체 2차원 배열 m의 value와 used 변수들을 초기화시킴.
+    반환값 : 없음
+*/
 
 int readMove() {
   int move, validMove = 0;
@@ -167,7 +208,9 @@ int readMove() {
     if (move >= 0 && move < 3) validMove = 1;
   }
   return move;
-}
+}/* 반환값 move : 입력 받은 키값 반환.
+    사용자의 키보드 입력값을 반환한다.
+*/
 
 int main() {
   struct Square board[SIZE][SIZE];
