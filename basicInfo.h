b8a28421b87687fe687b1eb80f33e68fb7f4dad6
zooh <zero5.two4@gmail.comm>
2019-05-29 22:12:53 +0900

    make cleancode at basicInfo version 1.1  On branch cleancode_basicInfo  Changes to be committed: 	modified:   basicInfo.h

--- basicInfo.h
+++ basicInfo.h
@@ -8,12 +8,12 @@ enum directions { RIGHT, DOWN, LEFT, UP };
 //directions 0 ->, 1 down,  2 <-, 3 up
 
 struct Square {
-	int value;
-	int used;
-};
+	int value;	// 사용자에게 보여주는 data이다.
+	int used;	// 사용자에게 보여주는 data이다.
+};/*2차원 Matrix형태로 square배열이 만들어진다. square하나가 cell 하나와 같다고 생각하면 된다.*/
 
 struct Pos {
 	int x;
 	int y;
-};
+};/* 2차원 Matrix형태로 sqaure배열의 cell 위치 data를 갖는다. */
 #endif
