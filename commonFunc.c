b7e692dabf7e65e37a333667fcd36cd2e8ea6aff
taeheuljeon <junth92@gmail.com>
2019-05-30 17:56:14 +0900

    Feature for issue #20 version 4 Merge branch 'feature/Iss20' into     developi  On branch feature/Iss20  Changes to be committed: 	modified:   commonFunc.c

--- commonFunc.c
+++ commonFunc.c
@@ -18,11 +18,11 @@ int playerWon(struct Square m[][SIZE]) {
 int readMove() {
 	char move;
 	int validMove = 0;
-	while (!validMove) {
+	while (isValidMove(validMove)) {
 		printf("UP : W, LEFT : A, DOWN : S, RIGHT : D\n");
 		printf("Move : ");
 		scanf("%c", &move);
-		while(getchar() != '\n');
+		while (getchar() != '\n');
 		if (move == 'w' || move == 'W') move = UP;
 		if (move == 'a' || move == 'A') move = LEFT;
 		if (move == 's' || move == 'S') move = DOWN;
@@ -32,27 +32,27 @@ int readMove() {
 	return move;
 }
 
-int isArrUsed(struct Square arr){
+int isArrUsed(struct Square arr) {
 	if (arr.used == 1)
 		return 1;
 	else
 		return 0;
 }
 
-int isEmptyValue(struct Square arr){
+int isEmptyValue(struct Square arr) {
 	if (arr.value == 0)
 		return 1;
 	else
 		return 0;
 }
 
-int isEqualofValue(struct Square A, struct Square B){
+int isEqualofValue(struct Square A, struct Square B) {
 	if (A.value == B.value)
 		return 1;
 	else
 		return 0;
 }
-int isPlayerLose(struct Squre m[][SIZE]){
+int isPlayerLose(struct Squre m[][SIZE]) {
 	struct Pos empty[SIZE*SIZE];
 	int cnt_empty = 0;
 	cnt_empty = getEmptyCells(empty, m);
@@ -65,14 +65,14 @@ int isPlayerLose(struct Squre m[][SIZE]){
 	}
 	else
 		return 0;
-	
+
 }
 
-int playerLose(struct Squre m[][SIZE]){
+int playerLose(struct Squre m[][SIZE]) {
 	struct Square temp_board[SIZE][SIZE];
 	struct Pos empty[SIZE*SIZE];
-	
-	for (int i = 0; i < 4; i++){
+
+	for (int i = 0; i < 4; i++) {
 		cpyBoard(temp_board, m);
 		handleMove(i, temp_board);
 		if (getEmptyCells(empty, temp_board) > 0)
@@ -82,11 +82,11 @@ int playerLose(struct Squre m[][SIZE]){
 	return 1;
 }
 
-void cpyBoard(struct Square temp[][SIZE], struct Square m[][SIZE]){
+void cpyBoard(struct Square temp[][SIZE], struct Square m[][SIZE]) {
 	// memcpy함수로 이용할 수 있으면 좋을 것 같다.
 
-	for (int i = 0; i < SIZE; i++){
-		for (int j = 0; j < SIZE; j++){
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
 			temp[i][j].used = m[i][j].used;
 			temp[i][j].value = m[i][j].value;
 		}
