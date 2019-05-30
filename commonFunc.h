d05e598fee86dd09999e4fef5d4faa7317e66b54
minjoo kim <min942773@gmail.com>
2019-05-30 20:04:25 +0900

    make cleancode at commonFunc.h version 1.2

--- commonFunc.h
+++ commonFunc.h
@@ -12,4 +12,5 @@ int isPlayerLose(struct Squre m[][SIZE]);
 int playerLose(struct Squre m[][SIZE]);
 void cpyBoard(struct Square temp[][SIZE], struct Square m[][SIZE]);
 int isValidMove(int validMove);
+int EmptyCellExists(struct Squre m[][SIZE]);
 #endif
